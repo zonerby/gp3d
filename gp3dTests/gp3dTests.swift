4d382ba882f2bd67cdda090ab33dc94e669f6495
Gavertyz <artyr4210@yandex.ru>
2017-07-05 16:26:14 +0300

    pisos
    initial commit

---
+++ gp3dTests/gp3dTests.swift
@@ -0,0 +1,36 @@
+//
+//  gp3dTests.swift
+//  gp3dTests
+//
+//  Created by Artur Artur on 05.07.17.
+//  Copyright © 2017 GP F. All rights reserved.
+//
+
+import XCTest
+@testable import gp3d
+
+class gp3dTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
