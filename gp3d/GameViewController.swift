f278144d212d01fdcdb91d8a6538adb256381574
Александр Лыков <Lykov.A.And@sberbank.ru>
2019-03-14 14:17:03 +0300

    - Изменено положение камеры и мяча - Рефакторинг экрана игры

--- gp3d/GameViewController.swift
+++ gp3d/GameViewController.swift
@@ -11,21 +11,24 @@ import QuartzCore
 import SceneKit
 import SpriteKit
 
-//enum sceneType: String {
-//    case Menu = "Menu"
-//    case Game = "Game"
-//}
+enum SceneType: String {
+    case menu = "Menu"
+    case game = "Game"
+}
+
+enum BallState: UInt {
+    case basic
+    case free
+}
 
 class GameViewController: UIViewController {
     
-    fileprivate var currentSceneName: String!
-    
-    fileprivate var ball: SCNNode!
-    
-    fileprivate var allowCameraControl: Bool = true
-    
-    private var menuHUDMaterial: SCNMaterial {
-        // Create a HUD label node in SpriteKit
+    private var currentSceneName: SceneType = .game
+    private var scene: SCNScene!
+    private var ball: SCNNode!
+    private var allowCameraControl: Bool = true
+    private var menuHUDMaterial: SCNMaterial
+    {
         let sceneSize = CGSize(width: 300, height: 200)
         
         let skScene = SKScene(size: sceneSize)
@@ -46,149 +49,147 @@ class GameViewController: UIViewController {
         return material
     }
     
-    private func prepare(to scene: SCNScene) {
-        switch currentSceneName {
-        case "Menu":
-            let hudNode = scene.rootNode.childNode(withName: "text", recursively: true)!
-            hudNode.geometry?.materials = [self.menuHUDMaterial]
-            hudNode.rotation = SCNVector4(x: 1, y: 0, z: 0, w: Float.pi) //try 2 replace Float.pi w/ 0
-            allowCameraControl = false
-        case "Game":
-            break
-        default:
-            print("eto defolt ty noob")
-        }
-    }
+    // MARK: - Superclass methods
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
-        // choose new scene type
-        //let menuScene = "Menu"
-        let gameScene = "Game"
-        
-        currentSceneName = gameScene
-        
-        let scene = SCNScene(named: "art.scnassets/\(currentSceneName.description).scn")!
-        
-        //custom preparations
-        prepare(to: scene)
-        
-        //default preparations
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        // set the scene to the view
-        scnView.scene = scene
-        
-        // allows the user to manipulate the camera
-        //scnView.allowsCameraControl = allowCameraControl
-        
-        // show statistics such as fps and timing information
-        scnView.showsStatistics = true
-        
-        // configure the view
-        scnView.backgroundColor = UIColor.green
-        
-        let ballscn = SCNScene(named: "art.scnassets/ball.scn")!
-        self.ball = ballscn.rootNode.childNode(withName: "BASKET_BALL", recursively: true)!
-        self.ball.scale = SCNVector3(x:0.15, y:0.15, z:0.15)
-        self.ball.position = SCNVector3(x:0,y:86,z:-20)
-        self.ball.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
-        self.ball.physicsBody?.mass = 0.6237
-        self.ball.physicsBody?.isAffectedByGravity = false
-        self.ball.physicsBody?.friction = 1.0
-        self.ball.physicsBody?.rollingFriction = 0.01
-        scnView.scene?.rootNode.addChildNode(self.ball)
-        scnView.scene?.physicsWorld.gravity = SCNVector3(0, -260, 0)
-        
-        let basket = scene.rootNode.childNode(withName: "basket", recursively: true)!
-        basket.physicsBody = SCNPhysicsBody(type: .kinematic, shape: nil)
-        // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        scnView.addGestureRecognizer(tapGesture)
-        
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
-        scnView.addGestureRecognizer(panGesture)
-        
-        //let swipeGesture = UISwipeGestureRecognizer(target: self, action: nil)
+        prepareGameScene()
     }
     
-    @objc func handlePan(_ gesture: UIGestureRecognizer)
+    override var shouldAutorotate: Bool
+    {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool
+    {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone
+        {
+            return .allButUpsideDown
+        }
+        else
+        {
+            return .all
+        }
+    }
+    
+    // MARK: - Private functions
+    // MARK: Game scene
+    
+    private func prepareGameScene()
+    {
+        currentSceneName = .game
+        
+        configureScene()
+        configureBall()
+        configureBasket()
+        configureGestures()
+    }
+    
+    private func configureScene()
+    {
+        guard let scene = SCNScene(named: "art.scnassets/\(currentSceneName.rawValue).scn") else {return}
+        scene.physicsWorld.gravity = SCNVector3(0, -260, 0)
+        self.scene = scene
+        
+        if let scnView = self.view as? SCNView
+        {
+            scnView.scene = scene
+            scnView.showsStatistics = true
+            scnView.backgroundColor = UIColor.green
+        }
+        
+        switch currentSceneName {
+        case .menu:
+            guard let hudNode = scene.rootNode.childNode(withName: "text", recursively: true) else {return}
+            hudNode.geometry?.materials = [self.menuHUDMaterial]
+            hudNode.rotation = SCNVector4(x: 1, y: 0, z: 0, w: Float.pi)
+            allowCameraControl = false
+        default:
+            break
+        }
+    }
+    
+    private func configureBall()
+    {
+        guard let ballscn = SCNScene(named: "art.scnassets/ball.scn"),
+            let ballNode = ballscn.rootNode.childNode(withName: "BASKET_BALL", recursively: true) else {return}
+        let baseScale: Float = 0.15
+        ballNode.scale = SCNVector3(x:baseScale, y:baseScale, z:baseScale)
+        let body = SCNPhysicsBody(type: .dynamic, shape: nil)
+        body.mass = 0.6237
+        body.friction = 1.0
+        body.rollingFriction = 0.01
+        ballNode.physicsBody = body
+        ball = ballNode
+        updateBall(with: .basic)
+        scene.rootNode.addChildNode(ball)
+    }
+    
+    private func updateBall(with state: BallState)
+    {
+        guard let physicsBody = ball.physicsBody else { return }
+        switch state
+        {
+        case .free:
+            physicsBody.isAffectedByGravity = true
+            break
+        default:
+            physicsBody.velocity = SCNVector3()
+            physicsBody.isAffectedByGravity = false
+            physicsBody.angularVelocity = SCNVector4()
+            ball.position = SCNVector3(x:0, y:35, z:-20)
+            break
+        }
+    }
+    
+    private func configureBasket()
+    {
+        guard let basket = scene.rootNode.childNode(withName: "basket", recursively: true) else {return}
+        basket.physicsBody = SCNPhysicsBody(type: .kinematic, shape: nil)
+    }
+    
+    private func configureGestures()
+    {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        view.addGestureRecognizer(tapGesture)
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        view.addGestureRecognizer(panGesture)
+    }
+    
+    // MARK: - Gestures handling
+    
+    @objc private func handleTap(_ gestureRecognize: UIGestureRecognizer)
+    {
+        updateBall(with: .basic)
+    }
+    
+    @objc private func handlePan(_ gesture: UIGestureRecognizer)
     {
         guard let panGesture = gesture as? UIPanGestureRecognizer else { return }
         
         switch panGesture.state {
         case .ended:
             let velocity = panGesture.velocity(in: self.view)
-            
-            self.ball.physicsBody?.isAffectedByGravity = true
-            
             let scale: Float = 70.0
-            let force = SCNVector3(x: Float(velocity.x)/scale, y: abs(Float(velocity.y))/scale , z: Float(velocity.y)/scale)
-            let position = SCNVector3(x: 0, y: 0, z: 0)
-            self.ball.physicsBody?.applyForce(force,
-                                              at: position, asImpulse: true)
+            let force = SCNVector3(x: Float(velocity.x)/scale,
+                                   y: abs(Float(velocity.y))/scale,
+                                   z: Float(velocity.y)/scale)
+            let position = SCNVector3(x: 0, y: -0.5, z: 0)
+            updateBall(with: .free)
+            ball.physicsBody?.applyForce(force,
+                                         at: position,
+                                         asImpulse: true)
             break
         default:
             break
-        }
-        
-    }
-    
-    @objc
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let node = hitResults[0].node
-            
-            self.ball.physicsBody?.velocity = SCNVector3(0, 0, 0)
-            self.ball.physicsBody?.isAffectedByGravity = false
-            self.ball.physicsBody?.angularVelocity = SCNVector4()
-            self.ball.position = SCNVector3(x:0,y:86,z:-20)
-            
-            // get its material
-            let material = node.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.red
-            
-            SCNTransaction.commit()
-        }
-    }
-    
-    override var shouldAutorotate: Bool {
-        return true
-    }
-    
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-    
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
         }
     }
 }
